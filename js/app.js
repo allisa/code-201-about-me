7e23b6ec624692b030d817104fce015da2bd6b7f
Allisa LeBeuf <al5906@gmail.com>
2018-08-01 15:33:20 -0700

    added js changes for lab-3

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 'use strict';
+//creating a variable for tracking correct answers
+var correctAnswer = 0;
+
 //asking user for their name
 var userName = prompt('Hello, what is your name?');
 alert('Hi, ' + userName + '. Let\'s play a guessing game.');
 
-//asking user series of 5 questions
+//asking user series of 5 questions for guessing game
 var questionDog = prompt('Do I have a dog?').toUpperCase();
 if (questionDog === 'YES' || questionDog === 'Y') {
   alert('Yep, I do! His name is Pooka and he is the best dog in the entire world.');
-  console.log(questionDog + '. The user was right, I do have a dog');
+  correctAnswer++;
+  console.log(questionDog + '. The user was right');
 }
 else if (questionDog === 'NO' || questionDog === 'N') {
   alert('Incorrect, I do have a dog. His nickname is Mr. Ruffertons.');
@@ -16,6 +20,7 @@ else if (questionDog === 'NO' || questionDog === 'N') {
 var questionState = prompt('Am I from Washington?').toUpperCase();
 if (questionState === 'NO' || questionState === 'N') {
   alert('Correct, I moved to Washington from Colorado 10 years ago');
+  correctAnswer++;
   console.log(questionState + '. The user was right');
 }
 else if (questionState === 'YES' || questionState === 'Y') {
@@ -26,6 +31,7 @@ else if (questionState === 'YES' || questionState === 'Y') {
 var questionVeg = prompt('Am I a vegetarian?').toUpperCase();
 if (questionVeg === 'YES' || questionVeg === 'Y') {
   alert('That is correct.');
+  correctAnswer++;
   console.log(questionVeg + '. The user is correct');
 }
 else if (questionVeg === 'NO' || questionVeg === 'N') {
@@ -33,22 +39,75 @@ else if (questionVeg === 'NO' || questionVeg === 'N') {
   console.log(questionVeg + '. The user was wrong');
 }
 
+var questionSiblings = prompt('Do I have any siblings?').toUpperCase();
+if (questionSiblings === 'NO' || questionSiblings === 'N') {
+  alert('Incorrect.');
+  console.log(questionSiblings + '. The user was wrong');
+}
+else if (questionSiblings === 'YES' || questionSiblings === 'Y') {
+  alert('Correct, but how many?');
+  correctAnswer++;
+  console.log(questionSiblings + '. The user was right');
+}
+
+var guessesLeft = 4;
+var timesThrough = 0;
+while(guessesLeft >= 0) {
+  console.log('Guesses left', guessesLeft, 'Times through', timesThrough++);
+  if (guessesLeft === 0) {
+    alert('Out of guesses, move to next question. The correct answer is 2.');
+    break;
+  }
+
+  var siblingGuess = prompt('how many siblings do i have?');
+
+  if (siblingGuess > 2) {
+    guessesLeft --;
+    alert('Too high, ' + guessesLeft + ' more guesses left.');
+  }
+
+  else if(siblingGuess < 2) {
+    guessesLeft --;
+    alert('Too low, ' + guessesLeft + ' more guesses left.');
+  }
+
+  else if(parseInt(siblingGuess) === 2) {
+    alert('Correct!');
+    correctAnswer++;
+    console.log('The user guessed correctly. 2 siblings');
+    break;
+  }
+  else {
+    break;
+  }
+}
+
 var questionColor = prompt('Do I have a favorite color?').toUpperCase();
 if (questionColor === 'YES' || questionColor === 'Y') {
-  alert('Yep! My favorite color is green.');
+  alert('Yep, I have a couple!');
+  correctAnswer++;
   console.log(questionColor + '. The user was right');
 }
 else if (questionColor === 'NO' || questionColor === 'N') {
-  alert('Wrong, my favorite color is green.');
+  alert('Wrong, everyone has at least one favorite color.');
   console.log(questionColor + '. The user was wrong');
 }
 
-var keepPlaying = prompt('Do you want to keep playing?').toUpperCase();
-if (keepPlaying === 'NO' || keepPlaying === 'N') {
-  alert('Good, I am all out of questions.');
-  console.log(keepPlaying + '. The user was wrong');
+var favColors = ['purple', 'green', 'blue', 'teal'];
+for ( var colorGuesses = 6; colorGuesses >0; colorGuesses--) {
+  var favColorGuess = prompt('Guess one of my favorite colors');
+
+  if (favColors.includes(favColorGuess)) {
+    alert('Yes, that is one of my favorite colors');
+    correctAnswer++;
+    console.log(favColorGuess + '. The user guessed correctly');
+    break;
+  }
+
+  else {
+    alert('No, guess again.');
+  }
 }
-else if (keepPlaying === 'YES' || keepPlaying === 'Y') {
-  alert('Too bad. I am all out of questions');
-  console.log(keepPlaying + '. The user was right');
-}
+
+alert('You got ' + correctAnswer + ' out of 7 answers correct. Thanks for playing!');
+console.log(correctAnswer);
