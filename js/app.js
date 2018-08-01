89eee470ede5f388ae3f90c57efdff07d78daa92
Allisa LeBeuf <al5906@gmail.com>
2018-08-01 15:55:09 -0700

    added comments to js file and some minor css changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
-//creating a variable for tracking correct answers
+//creating a variable to track correct answers
 var correctAnswer = 0;
 
 //asking user for their name
 var userName = prompt('Hello, what is your name?');
 alert('Hi, ' + userName + '. Let\'s play a guessing game.');
 
-//asking user series of 5 questions for guessing game
+//questions for yes or no answers
 var questionDog = prompt('Do I have a dog?').toUpperCase();
 if (questionDog === 'YES' || questionDog === 'Y') {
   alert('Yep, I do! His name is Pooka and he is the best dog in the entire world.');
@@ -41,7 +41,7 @@ else if (questionVeg === 'NO' || questionVeg === 'N') {
 
 var questionSiblings = prompt('Do I have any siblings?').toUpperCase();
 if (questionSiblings === 'NO' || questionSiblings === 'N') {
-  alert('Incorrect.');
+  alert('Incorrect. Don\'t worry, I\'ll let you guess how many I siblings I have.');
   console.log(questionSiblings + '. The user was wrong');
 }
 else if (questionSiblings === 'YES' || questionSiblings === 'Y') {
@@ -50,6 +50,9 @@ else if (questionSiblings === 'YES' || questionSiblings === 'Y') {
   console.log(questionSiblings + '. The user was right');
 }
 
+//have user guess how many siblings I have. If user guesses higher or lower
+//the prompt will ask 4 times. If user guesses correctly, loop will break and move on
+//if user enters anything besides a number, loop will break
 var guessesLeft = 4;
 var timesThrough = 0;
 while(guessesLeft >= 0) {
@@ -93,7 +96,10 @@ else if (questionColor === 'NO' || questionColor === 'N') {
   console.log(questionColor + '. The user was wrong');
 }
 
-var favColors = ['purple', 'green', 'blue', 'teal'];
+//prompt to have user guess a favorite color
+//user has 6 tries
+//if user guesses correctly, the loops breaks
+var favColors = ['purple', 'green', 'blue', 'teal', 'gray'];
 for ( var colorGuesses = 6; colorGuesses >0; colorGuesses--) {
   var favColorGuess = prompt('Guess one of my favorite colors');
 
@@ -109,5 +115,6 @@ for ( var colorGuesses = 6; colorGuesses >0; colorGuesses--) {
   }
 }
 
+//alert to give user the total number of correct answers they got
 alert('You got ' + correctAnswer + ' out of 7 answers correct. Thanks for playing!');
 console.log(correctAnswer);
